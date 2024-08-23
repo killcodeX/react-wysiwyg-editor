c3dbbf0c9b1d65878aaa277994ca85a83a3358b9
Aaquib Ahmed <aaquib.ahmed@impactanalytics.co>
2024-08-23 17:34:11 +0530

    bug: fixed few changes

--- rollup.config.js
+++ rollup.config.js
@@ -4,9 +4,10 @@ import babel from "@rollup/plugin-babel";
 import commonjs from "@rollup/plugin-commonjs";
 import peerDepsExternal from "rollup-plugin-peer-deps-external";
 import { terser } from "rollup-plugin-terser";
+import typescript from "@rollup/plugin-typescript"; // Import TypeScript plugin
 
 export default {
-  input: "src/Editor/index.tsx", // Entry point
+  input: "src/Editor/index.tsx", // Entry point for your component
   output: [
     {
       file: "dist/index.js", // CommonJS output
@@ -24,7 +25,11 @@ export default {
   plugins: [
     babel({
       exclude: "node_modules/**",
-      presets: ["@babel/preset-react", "@babel/preset-env"],
+      presets: [
+        "@babel/preset-react",
+        "@babel/preset-env",
+        "@babel/preset-typescript",
+      ],
       plugins: ["@babel/plugin-transform-runtime"],
       babelHelpers: "runtime",
     }),
@@ -32,6 +37,7 @@ export default {
     resolve(),
     commonjs(),
     terser(),
+    typescript({ tsconfig: "./tsconfig.json" }), // Compile TypeScript
   ],
   external: ["react", "react-dom"], // Do not bundle React and ReactDOM
 };
